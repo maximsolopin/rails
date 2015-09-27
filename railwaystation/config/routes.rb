3bbc1d5974c6e7dffb71f6970cc0256bcd9a6d70
maximsolopin <maxim.solopin@gmail.com>
2015-09-27 23:49:04 +0300

    added routes

--- railwaystation/config/routes.rb
+++ railwaystation/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :trains
+  resources :routes
+  resources :railway_stations
+
   get 'welcome/index'
 
   root 'welcome#index'
-  resources :railway_stations
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
